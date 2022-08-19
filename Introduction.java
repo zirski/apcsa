e7d871d7ccad0d697cc8db23f64c4d397724dec3
zirski <tobiasjhammond@gmail.com>
2022-08-19 12:50:04 -0700

    initial commit

---
+++ Introduction.java
@@ -0,0 +1,6 @@
+public class Introduction {
+    public static void main(String[] args){
+        System.out.println("My name is Toby");
+        System.out.println("I like short function names");
+    }
+}
