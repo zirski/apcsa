8b871cc89b7d1cdd5b0393651e08fc41e7629ed6
zirski <tobiasjhammond@gmail.com>
2022-08-21 12:43:48 -0700

    Added launch.json and Year.java

---
+++ Year.java
@@ -0,0 +1,7 @@
+public class Year {
+    public static void main(String[] args) {
+        int year = 2022;
+        System.out.print("the current year is ");
+        System.out.println(year);
+    }
+}
