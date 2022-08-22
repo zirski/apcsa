1d9caf0f6bd03756ba5727987c694040e5093112
zirski <tobiasjhammond@gmail.com>
2022-08-22 13:25:31 -0700

    Added Timeline.java

---
+++ Timeline.java
@@ -0,0 +1,10 @@
+public class Timeline {
+    public static void main(String[] args){
+        int[] ages = {6, 8, 10, 12, 14, 16};
+
+        for (int i = 0; i < ages.length; i++) {
+            System.out.println(ages[i]);
+            System.out.println();
+        }
+    }
+}
