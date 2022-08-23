4b52b7bcbc61685d46f1cfad34a10a7d0cad71da
zirski <tobiasjhammond@gmail.com>
2022-08-22 18:18:19 -0700

    added data headers and fixed excess vertical lines

--- Timeline.java
+++ Timeline.java
@@ -4,21 +4,28 @@ public class Timeline {
         int[] ages = {6, 8, 10, 12, 14, 16};
         String[] events = {
                             "12/10 - Drove speedboat for the first time in Australia", 
-                            "6/12 - Started taking piano lessons", 
-                            "8/14 - Stopped Horseback riding because I got too scared :/", 
-                            "4/16 - Went on a date with a girl for the first time",
-                            "7/18 - Visited Iceland with my dad",
-                            "5/21 - Made my first short movie"
+                            "06/12 - Started taking piano lessons", 
+                            "08/14 - Stopped Horseback ridig because I got too scared :/", 
+                            "04/16 - Went on a date with a girl for the first time",
+                            "07/18 - Visited Iceland with my dad",
+                            "05/21 - Made my first short movie"
                         };
+
+        System.out.println("\nAge\t|\tMM/YY - Description");
+        System.out.println("\t|\t");
 
         for (int i = 0; i < ages.length; i++) { 
             System.out.print(ages[i]);
             System.out.print("\t|\t");
             System.out.println(events[i]);
 
-            for (int j = 0; j < 4; j++) {
-                System.out.println();   
+            if (i < (ages.length - 1)) {
+                for (int j = 0; j < 4; j++) {
+                    System.out.println("\t|");   
+                }
             }
         }
+
+        System.out.println();
     }
 }
