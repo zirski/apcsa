18a28001eb46e6452f53917f3db37305b7fa5512
zirski <tobiasjhammond@gmail.com>
2022-08-24 17:42:19 -0700

    Initial commit

---
+++ RectangleTester.java
@@ -0,0 +1,11 @@
+public class RectangleTester {
+    public static void main(String[] args) {
+        Rectangle myRectangle = new Rectangle(5, 12);
+        
+        System.out.println(myRectangle);
+    }
+
+
+
+
+}
