ab69b8446026d9920652d3f70203cc1eb75d361b
zirski <tobiasjhammond@gmail.com>
2022-08-29 11:20:39 -0700

    Initial commit, Created `Salutations.java`

---
+++ Projects/Salutations/Salutations.java
@@ -0,0 +1,25 @@
+public class Salutations {
+    private String name;
+
+    public Salutations(String MyName) {
+        name = MyName;
+    }
+
+    public void addressLetter() {
+        System.out.println("Dear" + name);
+    }
+
+    public void signLetter() {
+        System.out.println("Sincerely," + name);
+    }
+
+    public void addressMemo() {
+        System.out.println("To whom it may concern");
+    }
+
+    public void signMemo() {
+        System.out.println("Best," + name);
+    }
+
+}
+
