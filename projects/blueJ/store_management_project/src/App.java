578e3b0b1cd8aec1ee4dbae18dcbb444a85d94a0
zirski <tobiasjhammond@gmail.com>
2022-10-30 13:47:40 -0700

    refactored for a more OOP approach to writing methods

--- projects/blueJ/store_management_project/src/App.java
+++ projects/blueJ/store_management_project/src/App.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
+        User myUser = new User(null);
 
         System.out.println("---------------------Title screen---------------------");
         System.out.println("\nOptions:\t1. Make new License");
 
-        usrHeading();
+        System.out.print("usr: ");
         Scanner sc = new Scanner(System.in);
         
         int i = sc.nextInt();
         if (i == 1) {
-            makeLicense();
+            myUser.makeLicense();
         } else {
             System.out.println("Please enter a valid option.");
         }
@@ -23,59 +23,8 @@ public class App {
         if (i == 1) {
             //do stuff
         } else {
-            checkLicense();
+            myUser.checkLicense();
         }
         sc.close();
-    }
-    
-    public static void usrHeading() {
-        System.out.print("usr: ");
-    }
-
-    public static void makeLicense() {
-        System.out.println("Please select products:");
-        System.out.println("\n1. Canvas\t2. Darkroom\t3. Marketplace\t4. Collage");
-        usrHeading();
-
-        Product[] products = new Product[4];
-
-        products[0] = new Product("canvas", 15.00);
-        products[1] = new Product("darkroom", 12.00);
-        products[2] = new Product("marketplace", 20.00);
-        products[3] = new Product("collage", 18.00);
-        
-        ArrayList<Integer> arliChoices = new ArrayList<Integer>(0);
-        
-        Scanner sc = new Scanner(System.in);
-        String choices = sc.nextLine();
-
-        Scanner strSc = new Scanner(choices);
-        while (strSc.hasNext()) {
-            arliChoices.add(strSc.nextInt());
-        }
-        strSc.close();
-
-        
-        Product[] myChoices = new Product[arliChoices.size()];
-        
-        for (int i = 0; i < arliChoices.size(); i++) {
-            myChoices[i] = products[arliChoices.get(i)];
-        }
-        
-        System.out.println("Please select your plan:");
-        System.out.println("\n1. Monthly\t2. Semiannually\t3. annually");
-        usrHeading();
-        
-        int choice = sc.nextInt();
-        sc.close();
-        
-        Plan myPlan = new Plan(choice);
-        License myLicense = new License(myChoices, myPlan);
-    }
-
-    public static void checkLicense() {
-        System.out.println("License:");
-        //System.out.println("Products:\t" + myLicense.toString());
-        
     }
 }
--- projects/blueJ/store_management_project/src/User.java
+++ projects/blueJ/store_management_project/src/User.java
@@ -1,7 +1,57 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class User {
     private License license;
 
     public User(License license) {
         this.license = license;
+    }
+
+    public void makeLicense() {
+        System.out.println("Please select products:");
+        System.out.println("\n1. Canvas\t2. Darkroom\t3. Marketplace\t4. Collage");
+        System.out.print("usr: ");
+
+        Product[] products = new Product[4];
+
+        products[0] = new Product("canvas", 15.00);
+        products[1] = new Product("darkroom", 12.00);
+        products[2] = new Product("marketplace", 20.00);
+        products[3] = new Product("collage", 18.00);
+        
+        ArrayList<Integer> arliChoices = new ArrayList<Integer>(0);
+        
+        Scanner sc = new Scanner(System.in);
+        String choices = sc.nextLine();
+
+        Scanner strSc = new Scanner(choices);
+        while (strSc.hasNext()) {
+            arliChoices.add(Integer.parseInt(strSc.next()));
+        }
+        strSc.close();
+
+        
+        Product[] myChoices = new Product[arliChoices.size()];
+        
+        for (int i = 0; i < arliChoices.size(); i++) {
+            myChoices[i] = products[arliChoices.get(i)];
+        }
+        
+        System.out.println("Please select your payment plan:");
+        System.out.println("\n1. Monthly\t2. Semiannually\t3. annually");
+        System.out.print("usr: ");
+        
+        int choice = sc.nextInt();
+        
+        Plan myPlan = new Plan(choice);
+        License myLicense = new License(myChoices, myPlan);
+        license = myLicense;
+    }
+
+    public void checkLicense() {
+        System.out.println("License:");
+        System.out.println("Products:\t" + license.toString());
+        
     }
 }
