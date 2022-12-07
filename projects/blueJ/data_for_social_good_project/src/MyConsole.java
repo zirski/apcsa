c445dcc9dc33f85a3e1bb4672e3f2adc7b0dd656
zirski <tobiasjhammond@gmail.com>
2022-12-06 20:25:00 -0800

    initial commit

---
+++ projects/blueJ/data_for_social_good_project/src/MyConsole.java
@@ -0,0 +1,5 @@
+public class MyConsole {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ projects/blueJ/data_for_social_good_project/src/Spotbox.java
@@ -0,0 +1,17 @@
+public class Spotbox {
+
+    // TO DO: Declare instance variables for the arrays you need.
+  
+  
+    
+    // TO DO: Write a constructor to initialize the instance variables.
+  
+    
+  
+    // TO DO: Write your methods to find the information you need about your data.
+  
+  
+  
+    
+    
+  }
