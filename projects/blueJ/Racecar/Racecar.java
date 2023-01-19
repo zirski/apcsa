e6e00caa5437527c7723774871729a650991cdae
zirski <tobiasjhammond@gmail.com>
2023-01-18 20:22:46 -0800

    final version

--- projects/blueJ/Racecar/Racecar.java
+++ projects/blueJ/Racecar/Racecar.java
@@ -12,7 +12,7 @@ public class Racecar {
     }
     
     public double computeTime() {
-        return 0.0;
+        return HUNTINGTON_CONSTANT * Math.cbrt(this.weight / this.power);
     }
     
     public String toString() {
@@ -23,21 +23,33 @@ public class Racecar {
     
     public static void main(String[] args) {
         // 1. Generate two random numbers between [2000, 5000] for weight
-        
+        int w1 = Randomizer.nextInt(2000, 5000);
+        int w2 = Randomizer.nextInt(2000, 5000);
         
         // 2. Generate two more random numbers between [500, 1000] for horsepower
-        
+        int hp1 = Randomizer.nextInt(500, 100);
+        int hp2 = Randomizer.nextInt(500, 100);
         
         // 3. Create two Racecar objects with distinct names, weight, and horsepower.
         //    You need to complete the constructor of the Racecar class.
-        
+        Racecar Webber = new Racecar("Webber", w1, hp1);
+        Racecar Senna = new Racecar("Senna", w2, hp2);
         
         // 4. Call the computeTime method on each object and print out the results.
         //    You need to complete the computeTime method in the Racecar class based
         //    on the assignment directions.
-        
+        double WebberET = Webber.computeTime();
+        double SennaET = Senna.computeTime();
         
         // 5. Print finishing times of both cars.
-        
+        System.out.println(Webber.name + ": ");
+        System.out.println("\tWeight: " + Webber.weight);
+        System.out.println("\tPower: " + Webber.power);
+        System.out.println("\tFinish time: " + WebberET + "\n");
+
+        System.out.println(Senna.name + ": ");
+        System.out.println("\tWeight: " + Senna.weight);
+        System.out.println("\tPower: " + Senna.power);
+        System.out.println("\tFinish time: " + SennaET);
     }
 }
